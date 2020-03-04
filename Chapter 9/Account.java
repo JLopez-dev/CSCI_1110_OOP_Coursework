fa37778fd9b4b9b9e372ec310d06371aaca515e4
John Lopez <56083360+JLopez-dev@users.noreply.github.com>
2020-03-04 11:41:18 -0700

    Add files via upload

---
+++ Chapter 9/Account.java
@@ -0,0 +1,68 @@
+/*
+ * Author: John Lopez.
+ * Date: 03/04/2020.
+ */
+
+package Account;
+
+public class Account {
+	
+	private int id = 0;
+	private double balance = 0;
+	private double annualInterestRate = 4.5;
+	private java.util.Date dateCreated;
+
+	double monthlyInterestRate = annualInterestRate / 1200;
+	
+	Account(){
+		
+	}
+	 
+	Account(int newId, double newBalance){
+		id = newId;
+		balance = newBalance;
+		dateCreated = new java.util.Date();
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	public double getAnnualInterestRate() {
+		return balance / 100;
+	}
+	
+	public java.util.Date getDateCreated() {
+		return dateCreated;
+	}
+	
+	double getMonthlyInterestRate() {
+		return monthlyInterestRate;
+		
+	}
+	
+	double getMonthlyInterest() {
+		return balance * monthlyInterestRate;
+	}
+	
+	double withdraw(double amount) {
+		return balance = balance - amount;
+	}
+	
+	double deposit(double amount) {
+		return balance = balance + amount;
+		
+	}
+}
---
+++ Chapter 9/TestAccount.java
@@ -0,0 +1,20 @@
+/*
+ * Author: John Lopez.
+ * Date: 03/04/2020.
+ */
+
+package Account;
+
+public class TestAccount {
+	public static void main(String[] args) {
+		
+		Account acc = new Account(1122, 20000);
+		
+		acc.withdraw(2500);
+		acc.deposit(3000);
+		System.out.printf("Balance: $%.2f%n", acc.getBalance());
+		System.out.printf("Monthly interest: $%.2f%n", acc.getMonthlyInterest());
+		System.out.println("Date when this account was created: " + acc.getDateCreated());
+		
+	}
+}

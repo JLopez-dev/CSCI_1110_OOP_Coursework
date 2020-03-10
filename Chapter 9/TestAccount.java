aaa74cc3fdf37231be0ba15a93659f56f11d89c1
John Lopez <56083360+JLopez-dev@users.noreply.github.com>
2020-03-10 09:05:39 -0600

    Update TestAccount.java

--- Chapter 9/TestAccount.java
+++ Chapter 9/TestAccount.java
@@ -9,6 +9,7 @@ public class TestAccount {
 	public static void main(String[] args) {
 		
 		Account acc = new Account(1122, 20000);
+		acc.annualInterestRate = 4.5;
 		
 		acc.withdraw(2500);
 		acc.deposit(3000);

2f43ff12b18338110638f3383f1edef318e38f0b
John Lopez <56083360+JLopez-dev@users.noreply.github.com>
2020-07-15 11:01:23 -0600

    Add files via upload

---
+++ Chapter 13/Exercise 13-1/Triangle.java
@@ -0,0 +1,35 @@
+/* Name: John Lopez.
+ * Date: 7/15/2020.
+ */
+
+package Triangle;
+
+public class Triangle {
+	private double side1 = 1.0;
+	private double side2 = 1.0;
+	private double side3 = 1.0;
+	
+	public double getSide1() {
+		return side1;
+	}
+	
+	public double getSide2() {
+		return side2;
+	}
+	
+	public double getSide3() {
+		return side3;
+	}
+	
+	public double getArea() {
+		return getArea();
+	}
+
+	public double getPerimeter() {
+		return getPerimeter();
+	}
+	
+	public String toString() {
+		return "Triangle: side1 = " + side1 + " side2  = " + side2 + " side3  = " + side3;
+	}
+}	

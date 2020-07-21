01c08e65b4ea33d0f700c81f6a4c4f97a5e3a9a9
John Lopez <56083360+JLopez-dev@users.noreply.github.com>
2020-07-21 12:13:39 -0600

    Add files via upload

---
+++ Chapter 13/Exercise 13-11/Octagon.java
@@ -0,0 +1,59 @@
+/* Name: John Lopez.
+ * Date: 7/15/2020.
+ */
+
+package Exercise13_11;
+
+public class Octagon extends GeometricObject implements Cloneable, Comparable<Octagon> {
+	
+	private double side;
+
+	public Octagon() {
+	}
+
+	
+	public Octagon(double side) {
+		this.side = side;
+	}
+
+	
+	public void setSide(double side) {
+		this.side = side;
+	}
+
+	
+	public double getSide() {
+		return side;
+	}
+
+	@Override
+	public double getArea() {
+		return (2 + 4 / Math.sqrt(2)) * side * side;
+	}
+
+	@Override
+	public double getPerimeter() {
+		return 8 * side;
+	}
+
+	@Override
+	public int compareTo(Octagon octg) {
+		if (getArea() > octg.getArea())
+			return 1;
+		else if (getArea() < octg.getArea())
+			return -1;
+		else
+			return 0;
+	}
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "\nArea: " + getArea() + 
+			"\nPerimeter: " + getPerimeter();
+	}
+}
